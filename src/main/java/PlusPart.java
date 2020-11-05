3d7ead5cf15838b90f3b75ded2fe75393d988856
bngel <bngel1024@gmail.com>
2020-11-06 00:23:34 +0800

    修复了部分JAVAb版本BUG

--- src/main/java/PlusPart.java
+++ src/main/java/PlusPart.java
@@ -1,30 +1,34 @@
-import com.sun.org.apache.xpath.internal.operations.Plus;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PlusPart implements Runnable{
-    static long ans = 0;
+    static ArrayList<PlusPart> plusParts = new ArrayList<>();
 
     private Thread thread;
-    private int start;
-    private int end;
+    private long start;
+    private long end;
     private int x;
     private String threadName;
+    private long ans;
 
-    PlusPart(int start,int end, int x,String name) {
+    PlusPart(long start,long end, int x,String name) {
         this.start = start;
         this.end = end;
         threadName = name;
         this.x = x;
-        ans = 0;
+        this.ans = 0;
+    }
+
+    public long getAns() {
+        return ans;
     }
 
     @Override
     public void run() {
-        for (int i=start;i<end;i++)
-            if (contain(i,x))
+        for (long i=start;i<end;i++)
+            if (contain(i,x)) {
                 ans += i;
+            }
     }
 
     public void start() {
@@ -34,23 +38,37 @@ public class PlusPart implements Runnable{
         }
     }
 
+    public boolean isAlive(){
+        return thread.isAlive();
+    }
+
     private static boolean contain(long num, int x) {
         return String.valueOf(num).contains(String.valueOf(x));
     }
 
+    private static boolean isFinished() {
+        for (PlusPart p: plusParts) {
+            if (p.isAlive()) return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        ArrayList<PlusPart> plusParts = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-        for (int i=0;i<1000000001;i+=1000000)
-            plusParts.add(new PlusPart(i,i+1000000,x,String.format("thread%d",i)));
+        long ans = 0;
+        for (long i=0;i<1000000001;i+=100000)
+            plusParts.add(new PlusPart(i,i+100000,x,String.format("thread%d",i)));
         for (PlusPart p: plusParts) {
             p.start();
             System.out.println("Thread:"+ p.threadName + " start");
         }
-        if (x == 0 || x == 1) {
-            ans -= 1000000000;
+        while(!isFinished());
+        for (PlusPart p: plusParts) {
+            ans += p.getAns();
         }
+        if (x == 0 || x == 1)
+            ans -= 1000000000;
         System.out.println(ans);
     }
 }
